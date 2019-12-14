dc19bff01cde4e01d64fb908187fc15b7321c8e7
Miquel <56106636+miquelperello@users.noreply.github.com>
2019-12-14 19:43:04 +0100

    Update bot1.py

--- Fonts/bot1.py
+++ Fonts/bot1.py
@@ -4,23 +4,42 @@ import telegram
 import time
 import webbrowser
 import random
+ 
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, CallbackQueryHandler,  MessageHandler, Filters
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from staticmap import StaticMap, CircleMarker
+from googletrans import Translator
 
 ############################### Bot ############################################
+
+translator = Translator() # Create object of Translator.
+
+
+language="EN"
+
+def translate(text):
+  if language == "ES":
+    translated = translator.translate(text, dest='es')
+    return translated
+  elif language == "EN":
+    translated = translator.translate(text, dest='en')
+    return translated
+  elif language =="CAT":
+    translated = translator.translate(text, dest='ca')
+    return translated
+  elif language =='FR':
+    translated = translator.translate(text, dest='fr')
+    return translated
+
 
 
 
 def start(bot, update):
-  webbrowser.open('http://inventwithpython.com/')
-  update.message.reply_text(main_menu_message_cat(), #1r param: missatge 
+  update.message.reply_text(translate(main_menu_message_cat()).text, #1r param: missatge 
                             reply_markup=main_menu_keyboard_cat())#2n param: botons 
-
-  #time.sleep(1.2)
  
 
 
@@ -28,7 +47,7 @@ def main_menu_cat(bot, update):
   query = update.callback_query
   bot.edit_message_text(chat_id=query.message.chat_id,
                         message_id=query.message.message_id, #1r param: missatge 
-                        text=main_menu_message_cat(),
+                        text=translate(main_menu_message_cat()).text,
                         reply_markup=main_menu_keyboard_cat()) #1r param: menú keyboard al que anem al pulsar 
 
 def link_menu_cat(bot, update):
@@ -42,29 +61,36 @@ def rrss_menu_cat(bot, update):
   query = update.callback_query
   bot.edit_message_text(chat_id=query.message.chat_id,
                         message_id=query.message.message_id,
-                        text=rrss_menu_message(),
+                        text=translate(rrss_menu_message()).text,
                         reply_markup=rrss_menu_keyboard_cat()) 
 
- 
+
+
+def sos_menu_cat(bot, update):
+  query = update.callback_query
+  bot.send_contact(chat_id=query.message.chat_id, phone_number= "112", first_name="Teléfono de emergencias")
+  update.message.reply_text(main_menu_message_cat(), #1r param: missatge 
+                            reply_markup=main_menu_keyboard_cat())
+
 
 ############################ Keyboards #########################################
 def main_menu_keyboard_cat():
-  keyboard = [[InlineKeyboardButton('Informació🔍', callback_data='info_keyboard_cat')],
-              [InlineKeyboardButton('Recursos♿', callback_data='recursos_keyboard_cat')],
-              [InlineKeyboardButton('Links d\'interès🌐', callback_data='link_menu_keyboard_cat')],
-              [InlineKeyboardButton('SOS📞', callback_data='sos_keyboard_cat')],
-              [InlineKeyboardButton('Test de concienciació📚', callback_data='test_keyboard_cat')],
-              [InlineKeyboardButton('Donatius🎉', callback_data='donatiu_keyboard_cat')]]
+  keyboard = [[InlineKeyboardButton(translate("Informació").text + "🔍", callback_data='info_keyboard_cat')],
+              [InlineKeyboardButton(translate("Recursos").text + "♿", callback_data='link_menu_keyboard_cat')],
+              [InlineKeyboardButton(translate("Links d\'interès").text + "🌐", callback_data='link_menu_keyboard_cat')],
+              [InlineKeyboardButton(translate("SOS").text+'📞', callback_data='sos_menu_cat')],
+              [InlineKeyboardButton(translate("Test de concienciación").text+'📚', callback_data='test_keyboard_cat')],
+              [InlineKeyboardButton(translate("Donatius").text+'🎉', callback_data='donatiu_keyboard_cat')]]
               #[InlineKeyboardButton('BACK', callback_data='BACK_Keyboard_cat')]
   return InlineKeyboardMarkup(keyboard)
 
 
 def link_menu_keyboard_cat():
-  keyboard = [[InlineKeyboardButton('Xarxes Socials📱', callback_data='rrss_menu_keyboard_cat')],
-              [InlineKeyboardButton('Associacions🚻', callback_data='main_menu_cat')],
-              [InlineKeyboardButton('Links d\'interès🌐', url = "https://www.share4rare.org/")],
-              [InlineKeyboardButton('Llibre la cigüeña añil📖', url="https://weeblebooks.com/es/educacion-emocional/la-ciguena-anil/")],
-              [InlineKeyboardButton('BACK🔙', callback_data='m1')]]
+  keyboard = [[InlineKeyboardButton(translate('Xarxes Socials').text + "📱", callback_data='rrss_menu_keyboard_cat')],
+              [InlineKeyboardButton(translate('Associacions').text + "🚻", callback_data='main_menu_cat')],
+              [InlineKeyboardButton(translate('Links d\'interès').text + "🌐", url = "https://www.share4rare.org/")],
+              [InlineKeyboardButton(translate('Llibre de la cigüeña añil').text+'📖', url="https://weeblebooks.com/es/educacion-emocional/la-ciguena-anil/")],
+              [InlineKeyboardButton(translate('BACK').text + " 🔙", callback_data='main_menu_cat')]]
   return InlineKeyboardMarkup(keyboard)
 
 def rrss_menu_keyboard_cat():
@@ -73,7 +99,7 @@ def rrss_menu_keyboard_cat():
               [InlineKeyboardButton('Facebook', url = "https://bit.ly/2PHPZr6")],
               [InlineKeyboardButton('LinkedIn', url="https://www.linkedin.com/company/share4rare")],
               [InlineKeyboardButton('WhatsApp', url="https://bit.ly/36ArtyU")],
-              [InlineKeyboardButton('BACK🔙', callback_data='rrss_menu_keyboard_cat')]]
+              [InlineKeyboardButton('BACK🔙', callback_data='link_menu_keyboard_cat')]]
   return InlineKeyboardMarkup(keyboard)  
 
 
@@ -84,7 +110,13 @@ def link_link_rrss():
                  parse_mode=telegram.ParseMode.MARKDOWN)  
 
 
-"""
+#########################EXTRA#############################
+
+def echo(bot, update):
+  print(update.message.text)
+  bot.send_message(chat_id=update.message.chat_id, text=translator.translate(update.message.text))
+
+
 
 def where(bot, update, user_data):
     try:
@@ -92,6 +124,8 @@ def where(bot, update, user_data):
         lat, lon = update.message.location.latitude, update.message.location.longitude
         mapa = StaticMap(500, 500)
         mapa.add_marker(CircleMarker((lon, lat), 'blue', 10))
+        mapa.add_marker(CircleMarker((-3.7025600, 40.4165000), 'red', 10))
+        mapa.add_marker(CircleMarker((9.0000000, 51.0000000), 'red', 10))
         imatge = mapa.render()
         imatge.save(fitxer)
         bot.send_photo(chat_id=update.message.chat_id, photo=open(fitxer, 'rb'))
@@ -99,27 +133,27 @@ def where(bot, update, user_data):
     except Exception as e:
         print(e)
         bot.send_message(chat_id=update.message.chat_id, text='💣') 
-"""
 
+"""
 def where(bot, update, user_data):
     lat, lon = update.message.location.latitude, update.message.location.longitude
     print(lat, lon)
     bot.send_message(chat_id=update.message.chat_id, text='Ets a les coordenades %f %f' % (lat, lon))   
-
+"""
 # and so on for every callback_data option
 
 ############################# Messages #########################################
 def main_menu_message_cat():
-  return 'Hola! Benvingut a RareBot!\n Pots buscar Informació🔍 d\'enfermetats minoritàries, buscar Material♿, veure els Links d\'interès🌐, fer  un Test de concienciació📚 o fer Donatius🎉! ")'
+  return "Hola! Benvingut a RareBot!\n Pots buscar Informació d\'enfermetats minoritàries, buscar Material, veure els Links d\'interès, fer  un Test de concienciació o fer Donatius!"
 
 def link_menu_message_cat():
-  return 'Escull què vols!'
+  return "Escull què vols!"
 
 def second_menu_message():
   return 'Choose the submenu in second menu:'
 
 def link_menu_message():
-  return 'Aquí pots trobar diferents links d \'interès🌐'
+  return 'Aquí pots trobar diferents links d \'interès'
 
 
 def rrss_menu_message():
@@ -136,11 +170,14 @@ updater.dispatcher.add_handler(CallbackQueryHandler(main_menu_cat, pattern='main
 updater.dispatcher.add_handler(CallbackQueryHandler(link_menu_cat, pattern='link_menu_keyboard_cat'))
 updater.dispatcher.add_handler(CallbackQueryHandler(link_link_rrss, pattern='m4'))
 updater.dispatcher.add_handler(CallbackQueryHandler(rrss_menu_cat, pattern='rrss_menu_keyboard_cat'))
+updater.dispatcher.add_handler(CallbackQueryHandler(sos_menu_cat))
 updater.dispatcher.add_handler(MessageHandler(Filters.location, where, pass_user_data=True))
+updater.dispatcher.add_handler(CommandHandler("help", help))
+  
 
 updater.start_polling()
 ################################################################################
-
+updater.idle()
 
 
 
